8ef857b9ee4d23898f75f2c505eb649368ec9476
JQKER10 <khanhmlll2k6@gmail.com>
2025-10-25 11:17:47 +0700

    Đã khởi tạo folder, model, preprocessing, training

---
+++ model/mamba2_model.py
@@ -0,0 +1,161 @@
+from mamba_utils import Mlp, ConvLayer
+import torch
+import torch.nn as nn
+from einops import rearrange, repeat
+import math
+
+
+class SSD(nn.Module):
+   def __init__(
+        self,
+        d_model,
+        d_conv=3, #default to 3 for 2D
+        conv_init=None,
+        expand=2,
+        headdim=64, #default to 64
+        ngroups=1,
+        A_init_range=(1, 16),
+        dt_min=0.001,
+        dt_max=0.1,
+        dt_init_floor=1e-4,
+        dt_limit=(0.0, float("inf")),
+        learnable_init_states=False,
+        activation="silu", #default to silu
+        bias=False,
+        conv_bias=True,
+        # Fused kernel and sharding options
+        chunk_size=256,
+        use_mem_eff_path=False, #default to False, for custom implementation
+        layer_idx=None,  # Absorb kwarg for general module
+        device=None,
+        dtype=None,
+        linear_attn_duality=False,
+        d_state = 64,
+        **kwargs
+    ):
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.d_model = d_model
+        self.d_conv = d_conv
+        self.conv_init = conv_init
+        self.expand = expand
+        self.d_inner = int(self.expand * self.d_model)
+        self.headdim = headdim
+        self.d_state = d_state
+        if ngroups == -1:
+            ngroups = self.d_inner // self.headdim #equivalent to multi-head attention
+        self.ngroups = ngroups
+        assert self.d_inner % self.headdim == 0
+        self.nheads = self.d_inner // self.headdim
+        self.dt_limit = dt_limit
+        self.learnable_init_states = learnable_init_states
+        self.activation = activation
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.d_model = d_model
+        self.d_conv = d_conv
+        self.conv_init = conv_init
+        self.expand = expand
+        self.d_inner = int(self.expand * self.d_model)
+        self.headdim = headdim
+        self.d_state = d_state
+        if ngroups == -1:
+            ngroups = self.d_inner // self.headdim #equivalent to multi-head attention
+        self.ngroups = ngroups
+        assert self.d_inner % self.headdim == 0
+        self.nheads = self.d_inner // self.headdim
+        self.dt_limit = dt_limit
+        self.learnable_init_states = learnable_init_states
+        self.activation = activation
+        #convert chunk_size to triton.language.int32
+        self.chunk_size = chunk_size#torch.tensor(chunk_size,dtype=torch.int32)
+        self.use_mem_eff_path = use_mem_eff_path
+        self.layer_idx = layer_idx
+        self.ssd_positve_dA = kwargs.get('ssd_positve_dA', True) #default to False, ablation for linear attn duality
+        # Order: [z, x, B, C, dt]
+        d_in_proj = 2 * self.d_inner + 2 * self.ngroups * self.d_state + self.nheads
+        self.in_proj = nn.Linear(self.d_model, int(d_in_proj), bias=bias, **factory_kwargs) #
+
+        conv_dim = self.d_inner + 2 * self.ngroups * self.d_state
+
+
+        self.conv2d = nn.Conv2d(
+            in_channels=conv_dim,
+            out_channels=conv_dim,
+            groups=conv_dim,
+            bias=conv_bias,
+            kernel_size=d_conv,
+            padding=(d_conv - 1) // 2,
+            **factory_kwargs,
+        )
+
+        if self.conv_init is not None:
+            nn.init.uniform_(self.conv1d.weight, -self.conv_init, self.conv_init)
+        # self.conv1d.weight._no_weight_decay = True
+
+        if self.learnable_init_states:
+            self.init_states = nn.Parameter(torch.zeros(self.nheads, self.headdim, self.d_state, **factory_kwargs))
+            self.init_states._no_weight_decay = True
+
+        self.act = nn.SiLU()
+
+        # Initialize log dt bias
+        dt = torch.exp(
+            torch.rand(self.nheads, **factory_kwargs) * (math.log(dt_max) - math.log(dt_min))
+            + math.log(dt_min)
+        )
+        dt = torch.clamp(dt, min=dt_init_floor)
+        # Inverse of softplus: https://github.com/pytorch/pytorch/issues/72759
+        inv_dt = dt + torch.log(-torch.expm1(-dt))
+        self.dt_bias = nn.Parameter(inv_dt)
+        # Just to be explicit. Without this we already don't put wd on dt_bias because of the check
+        # name.endswith("bias") in param_grouping.py
+        self.dt_bias._no_weight_decay = True
+
+        # A parameter
+        assert A_init_range[0] > 0 and A_init_range[1] >= A_init_range[0]
+        A = torch.empty(self.nheads, dtype=torch.float32, device=device).uniform_(*A_init_range)
+        A_log = torch.log(A).to(dtype=dtype)
+        self.A_log = nn.Parameter(A_log)
+        # self.register_buffer("A_log", torch.zeros(self.nheads, dtype=torch.float32, device=device), persistent=True)
+        self.A_log._no_weight_decay = True
+
+        # D "skip" parameter
+        self.D = nn.Parameter(torch.ones(self.nheads, device=device))
+        self.D._no_weight_decay = True
+
+        # modified from RMSNormGated to layer norm
+        #assert RMSNormGated is not None
+        #self.norm = RMSNormGated(self.d_inner, eps=1e-5, norm_before_gate=False, **factory_kwargs)
+        self.norm = nn.LayerNorm(self.d_inner)
+        self.out_proj = nn.Linear(self.d_inner, self.d_model, bias=bias, **factory_kwargs)
+
+        #linear attention duality
+        self.linear_attn_duality = linear_attn_duality
+        self.kwargs = kwargs
+    def forward(self,u,H, ):
+       bcdt=self.in_proj(u)
+       B, C, 
+class MultiKernelConv(nn.Module):
+    def __init__(self, in_channels:int, out_channels:int, kernel_sizes=[3,5,7], stride=1, padding=0, dilation=1, groups=1,
+                 bias=True, dropout=0, norm=nn.BatchNorm2d, act_func=nn.ReLU):
+        super(MultiKernelConv, self).__init__()
+        self.convs = nn.ModuleList()
+        for k in kernel_sizes:
+            conv_layer = ConvLayer(
+                in_channels,
+                out_channels,
+                kernel_size=k,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+                groups=groups,
+                bias=bias,
+                dropout=dropout,
+                norm=norm,
+                act_func=act_func
+            )
+            self.convs.append(conv_layer)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        con_outputs=torch.concat([conv(x) for conv in self.convs], dim=1)
+        con_outputs=
---
+++ model/mamba_utils.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.utils.checkpoint as checkpoint
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class ConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, dilation=1, groups=1,
+                 bias=True, dropout=0, norm=nn.BatchNorm2d, act_func=nn.ReLU):
+        super(ConvLayer, self).__init__()
+        self.dropout = nn.Dropout3d(dropout, inplace=False) if dropout > 0 else None
+        self.conv = nn.Conv3d(
+            in_channels,
+            out_channels,
+            kernel_size=(kernel_size, kernel_size),
+            stride=(stride, stride),
+            padding=(padding, padding),
+            dilation=(dilation, dilation),
+            groups=groups,
+            bias=bias,
+        )
+        self.norm = norm(num_features=out_channels) if norm else None
+        self.act = act_func() if act_func else None
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.dropout is not None:
+            x = self.dropout(x)
+        x = self.conv(x)
+        if self.norm:
+            x = self.norm(x)
+        if self.act:
+            x = self.act(x)
+        return x
